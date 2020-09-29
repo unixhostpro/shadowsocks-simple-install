039e9608b7d70860c5544d0723aadf9f2be657d6
root <root@srv143400.xyzservers.net>
2020-09-29 12:01:43 +0000

    fix

--- shadowsocks-simple-install.sh
+++ shadowsocks-simple-install.sh
@@ -18,16 +18,22 @@ function ufw_port() {
 	fi
 }
 
+function generate_hash() {
+	echo -n "$1":"$2" | base64
+}
+
 function config() {
 	echo
+	echo "---------------------------------------"
 	echo "Your shadowsocks proxy configuration:"
 	echo "URL: ss://$( generate_hash chacha20-ietf-poly1305 $1 )@$( get_external_address ):$2"
+	echo "---------------------------------------"
 #	echo "Android client: https://play.google.com/store/apps/details?id=com.github.shadowsocks"
 #	echo "Clients for other devices: https://shadowsocks.org/en/download/clients.html"
 }
 
-apt update 
-apt install -y shadowsocks-libev # install shadowsocks 
+apt update
+apt install -y shadowsocks-libev # install shadowsocks
 mkdir -p /etc/shadowsocks-libev # ceate config directory
 config /etc/shadowsocks-libev/config.json $2 $3
 ufw_port $2
